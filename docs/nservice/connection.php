90933e65b364779f5837ce04f8a73cfa34facb4e
Charly Abraham <charly@core.ai>
2023-01-23 16:10:16 +0530

    first commit

--- docs/nservice/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_name = "bmibedyu_service";
-$mysql_username = "bmibedyu_bmi";
-$mysql_password = "7xWf7hFxLXWR";
-$server_name = "localhost:3306";
-$con = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
-?>
--- docs/service/footer.php
+++
@@ -1,2 +0,0 @@
- <p><a href="dashboard.php">Homepage</a> | <a href="contact.php">Contact</a> |  <a href="#">Disclaimer</a> <br />
-&copy; Copyright 2021 Chris IT Services, Design: Shine and Team </p>
